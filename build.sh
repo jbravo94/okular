836ca1ed9a63c4850a013b50ba4e652312e2bfb4
Johannes HEINZL <jh.heinzl@gmail.com>
2023-08-20 00:25:43 +0200

    Updates.

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
 (docker-compose exec -it --workdir /okular/build okular make) && (cd build && make install && source prefix.sh) && ./install/bin/okular
+
+# Run cmake from docker
+# make from docker and source and make install
+# run source and make install from host
